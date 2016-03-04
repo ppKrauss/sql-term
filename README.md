7d210e0b16eccd091976a96f0803311fcafbab1c
Peter Krauss <ppkrauss@gmail.com>
2016-03-04 19:20:50 -0300

    SQL revised

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-...
-
-Illustrates the use of [PostgreSQL textsearch-dictionaries](http://www.postgresql.org/docs/9.1/static/textsearch-dictionaries.html), etc.
+This project illustrates the use of [PostgreSQL textsearch-dictionaries](http://www.postgresql.org/docs/9.1/static/textsearch-dictionaries.html), [dict-xsyn](http://www.postgresql.org/docs/current/static/dict-xsyn.html), metaphone, and a simple database for terminological storage.
 
 # Term
-[Controlled terminologies](https://www.wikidata.org/wiki/Q1469824) ...
+... [Controlled terminologies](https://www.wikidata.org/wiki/Q1469824) ...
 
-XML  [JATS](https://en.wikipedia.org/wiki/Journal_Article_Tag_Suite), [AKN](http://www.akomantoso.org/) or [LexML](http://projeto.lexml.gov.br/documentacao/Parte-3-XML-Schema.pdf)... and public standard IDs as  [DOI](https://www.wikidata.org/wiki/Q25670) or [URN Lex](https://en.wikipedia.org/wiki/Lex_(URN)).
+XML: terms into [JATS](https://en.wikipedia.org/wiki/Journal_Article_Tag_Suite), [AKN](http://www.akomantoso.org/) or [LexML](http://projeto.lexml.gov.br/documentacao/Parte-3-XML-Schema.pdf) documents... Each document must use a public standard ID, as  [DOI](https://www.wikidata.org/wiki/Q25670) or [URN Lex](https://en.wikipedia.org/wiki/Lex_(URN)).
 
-... illustring use of [português brasileiro](https://www.wikidata.org/wiki/Q750553)...
+... lexemes ...  illustring use of [português brasileiro](https://www.wikidata.org/wiki/Q750553)... 
 
 ## Objetive ##
 ...
@@ -21,6 +19,11 @@ UML class diagram of [ini.sql](src/ini.sql):
 
 ```
 
+### NOTES
 
+* [Using metaphone as lexeme](http://stackoverflow.com/questions/4001579/postgresql-full-text-search-randomly-dropping-lexemes).
 
+* PostreSQL guide, 
+** [Text search dictionaries](http://www.postgresql.org/docs/9.1/static/textsearch-dictionaries.html#TEXTSEARCH-THESAURUS)
+** [runtime-config-client](http://www.postgresql.org/docs/current/static/runtime-config-client.html#GUC-DEFAULT-TEXT-SEARCH-CONFIG)
 
--- src/ini.sql
+++ src/ini.sql
@@ -40,6 +40,9 @@ CREATE TABLE term0.term(
   UNIQUE(fk_ns,term)
 );
 
+CREATE INDEX term_idx ON term0.term(term);
+CREATE INDEX term_metaphone_idx ON term0.term(kx_metaphone);
+
 
 CREATE FUNCTION term0.normalizename() RETURNS trigger AS $f$
 -- 
@@ -64,7 +67,21 @@ CREATE TRIGGER check_term
 -- --  LIB  --
 -- -- -- -- --
 
-CREATE FUNCTION term0.find(
+-- PRIVATE --
+CREATE FUNCTION term0.multimetaphone(text,int DEFAULT 6,varchar DEFAULT ' ') 
+RETURNS text AS $f$
+	SELECT 	 string_agg(metaphone(w,$2),$3) 
+	FROM regexp_split_to_table($1, E'[\\s;\\|]+') AS t(w);
+$f$ LANGUAGE sql;
+
+
+-- PUBLIC --
+
+
+CREATE FUNCTION term0.search(
+--
+-- Search terms by a query string. 
+--
 	text,                      -- 1. query string (usual term or term-fragments)
 	char DEFAULT '&', 	   -- 2. '&','|' or free or like-search ('%' or 'p' for prefix)
 	int DEFAULT 100,           -- 3. LIMIT
@@ -92,6 +109,77 @@ CREATE FUNCTION term0.find(
 		) END
 	;
 $f$ LANGUAGE sql;
+
+
+CREATE FUNCTION term0.search_metaphone(
+--
+-- Same as term0.search() but using metaphone.
+--
+	text,                      -- 1. query string (usual term or term-fragments)
+	char DEFAULT '&', 	   -- 2. '=', '&','|' or free or like-search ('%' or 'p' for prefix)
+	int DEFAULT 100,           -- 3. LIMIT
+        smallint DEFAULT 1,        -- 4. Namespace
+	int DEFAULT 6              -- 5. metaphone-length	
+) RETURNS varchar[] AS $f$
+	SELECT CASE WHEN $2='e' or $2='=' THEN (  -- exact
+			SELECT array_agg(term) 
+			FROM term0.term 
+			WHERE fk_ns=$4 AND kx_metaphone = term0.multimetaphone($1)
+			LIMIT $3
+		) WHEN $2='%' OR $2='p' OR $2='s' THEN (  -- 's'equence or 'p'refix
+			SELECT array_agg(term) 
+			FROM term0.term 
+			WHERE fk_ns=$4 AND kx_metaphone LIKE (
+				CASE WHEN $2='p' THEN '' ELSE '%' END || term0.multimetaphone($1,$5,'%') || '%'
+			) LIMIT $3
+		) ELSE (  -- pending, needs also "free tsquery" in $2 for '!' use and complex expressions
+			SELECT array_agg(term) 
+			FROM term0.term 
+			WHERE fk_ns=$4 AND to_tsvector('simple',kx_metaphone) @@ to_tsquery(
+				'simple',
+				term0.multimetaphone($1,$5,$2)
+			) LIMIT $3
+		) END;
+$f$ LANGUAGE sql;
+
+
+CREATE FUNCTION term0.find(
+--
+-- Find a term, or "nearest term", by an heuristic of search strategies. 
+--
+	text,                      -- 1. query string (usual term or term-fragments)
+	int DEFAULT 100,           -- 2. LIMIT
+        smallint DEFAULT 1,        -- 3. Namespace
+	int DEFAULT 6              -- 4. metaphone-length	
+-- FALTA tipo de search! 
+) RETURNS varchar[] AS $f$
+DECLARE
+  items text[];
+BEGIN
+	items := term0.search($1,'=',$2,$3);
+	IF array_length(items,1)=1 THEN
+		RETURN items;
+	END IF;
+	items := term0.search($1,'&',$2,$3);
+	IF array_length(items,1)>0 THEN
+		RETURN items;
+	END IF;
+	items := term0.search($1,'p',$2,$3);
+	IF array_length(items,1)>0 THEN
+		RETURN items;
+	END IF;
+	items := term0.search_metaphone($1,'=',$2,$3,$4);
+	IF array_length(items,1)>0 THEN
+		RETURN items;
+	END IF;
+	items := term0.search_metaphone($1,'&',$2,$3,$4);
+	IF array_length(items,1)>0 THEN
+		RETURN items;
+	END IF;
+	items := term0.search_metaphone($1,'p',$2,$3,$4);
+	return items;
+END;
+$f$ LANGUAGE plpgsql;
 
 
 --
